55df39dd457d32cf3761bcc2d1e353fd7e010873
MERN-TRAINER <futuralabs.mern@gmail.com>
2025-04-12 10:16:32 +0530

    external css

--- React/src/App.jsx
+++ React/src/App.jsx
@@ -3,9 +3,13 @@
 import { styles } from "./style"
 
 const App = () => {
-    return <div style={styles.container}>
-        <h1 style={styles.header}>Hello World</h1>
-        <p style={styles.paragraph}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur temporibus in fuga placeat magnam aperiam. Ipsum dolores voluptates vitae. Dignissimos corporis officiis porro sunt ab dolor, soluta aliquam doloremque aut.</p>
+    return <div >
+        {/* <h1 style={styles.header}>Hello World</h1>
+        <p style={styles.paragraph}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur temporibus in fuga placeat magnam aperiam. Ipsum dolores voluptates vitae. Dignissimos corporis officiis porro sunt ab dolor, soluta aliquam doloremque aut.</p> */}
+        <div className="container">
+            <h3 className="text-red">SAMPLE HEADING</h3>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci id laborum velit repudiandae labore sapiente, non totam sunt quo, impedit voluptate! Nostrum, maxime! Consequatur itaque nobis nesciunt numquam, corporis aut.</p>
+        </div>
     </div>
 }
 
