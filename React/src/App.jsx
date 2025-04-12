1ea16b7ebfa69c3e1de929668a7d666623d43ec9
MERN-TRAINER <futuralabs.mern@gmail.com>
2025-04-12 10:38:36 +0530

    external css

--- React/src/App.jsx
+++ React/src/App.jsx
@@ -1,16 +1,9 @@
 // inline, external, framework - bootstrap
 
-import { styles } from "./style"
+import HomePage from "./pages/HomePage"
 
 const App = () => {
-    return <div >
-        {/* <h1 style={styles.header}>Hello World</h1>
-        <p style={styles.paragraph}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur temporibus in fuga placeat magnam aperiam. Ipsum dolores voluptates vitae. Dignissimos corporis officiis porro sunt ab dolor, soluta aliquam doloremque aut.</p> */}
-        <div className="container">
-            <h3 className="text-red">SAMPLE HEADING</h3>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci id laborum velit repudiandae labore sapiente, non totam sunt quo, impedit voluptate! Nostrum, maxime! Consequatur itaque nobis nesciunt numquam, corporis aut.</p>
-        </div>
-    </div>
+    return <HomePage />
 }
 
 export default App
