01b4943e0ec8b52a9c133e72a06810b9fdc1dba1
MERN-TRAINER <futuralabs.mern@gmail.com>
2025-04-12 09:59:46 +0530

    react inline styling

---
+++ React/src/style.js
@@ -0,0 +1,14 @@
+export const styles = {
+    container: {
+        backgroundColor: "black",
+        color: "#fff",
+        padding: "10px",
+        fontFamily: "Arial"
+    },
+    header: {
+        color: "red"
+    },
+    paragraph: {
+        fontStyle: "italic"
+    }
+}
