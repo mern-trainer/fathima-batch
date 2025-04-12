188c40e25162b5f173e4fff71877a0698e63ab77
MERN-TRAINER <futuralabs.mern@gmail.com>
2025-04-12 09:59:46 +0530

    react inline styling

---
+++ React/src/App.jsx
@@ -0,0 +1,12 @@
+// inline, external, framework - bootstrap
+
+import { styles } from "./style"
+
+const App = () => {
+    return <div style={styles.container}>
+        <h1 style={styles.header}>Hello World</h1>
+        <p style={styles.paragraph}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur temporibus in fuga placeat magnam aperiam. Ipsum dolores voluptates vitae. Dignissimos corporis officiis porro sunt ab dolor, soluta aliquam doloremque aut.</p>
+    </div>
+}
+
+export default App
