d1fb94e9a426e2e3ff8226c4e6f4d6adb5f5a296
MERN-TRAINER <futuralabs.mern@gmail.com>
2025-04-17 10:18:24 +0530

    basic + upto cards

--- React/src/App.jsx
+++ React/src/App.jsx
@@ -1,9 +1,175 @@
 // inline, external, framework - bootstrap
 
-import HomePage from "./pages/HomePage"
+import { Fragment } from "react"
+import { Button, Card, Carousel, Container, Image, Nav, Navbar, NavDropdown } from "react-bootstrap"
+
 
 const App = () => {
-    return <HomePage />
+    return <Fragment>
+        <Navbar expand="md" bg="dark" className="navbar-dark">
+            <Container fluid>
+                <Navbar.Brand href="#home">
+                    <Image src="https://www.7up.com/images/7up-logo-large.png" alt="7up" height={50}/>
+                </Navbar.Brand>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav className="me-auto">
+                        <Nav.Link href="#home">Home</Nav.Link>
+                        <Nav.Link href="#link">Link</Nav.Link>
+                        <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+                            <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+                            <NavDropdown.Item href="#action/3.2">
+                                Another action
+                            </NavDropdown.Item>
+                            <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+                            <NavDropdown.Divider />
+                            <NavDropdown.Item href="#action/3.4">
+                                Separated link
+                            </NavDropdown.Item>
+                        </NavDropdown>
+                    </Nav>
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
+
+
+        <Carousel >
+            <Carousel.Item interval={1000}>
+                <Image className="w-100" src="https://www.7up.com/images/simple-7up/tropical-header.png" alt="7up-banner-1" />
+            </Carousel.Item>
+            <Carousel.Item interval={3000}>
+                <Image className="w-100" src="https://www.7up.com/images/simple-7up/image2.png" alt="7up-banner-2" />
+            </Carousel.Item>
+            <Carousel.Item interval={500}>
+                <Image className="w-100" src="https://www.7up.com/images/simple-7up/image1.png" alt="7up-banner-3" />
+            </Carousel.Item>
+        </Carousel>
+
+        <div className="mt-2 d-flex gap-2 overflow-auto">
+            <Card style={{ width: '18rem' }} className="flex-shrink-0">
+                <Card.Img variant="top" src="https://cdn.prod.website-files.com/62d84e447b4f9e7263d31e94/6399a4d27711a5ad2c9bf5cd_ben-sweet-2LowviVHZ-E-unsplash-1.jpeg" />
+                <Card.Body>
+                    <Card.Title>Card Title</Card.Title>
+                    <Card.Text>
+                    Some quick example text to build on the card title and make up the
+                    bulk of the card's content.
+                    </Card.Text>
+                    <Button variant="success" className="w-100">Go somewhere</Button>
+                </Card.Body>
+            </Card>
+            <Card style={{ width: '18rem' }} className="flex-shrink-0">
+                <Card.Img variant="top" src="https://cdn.prod.website-files.com/62d84e447b4f9e7263d31e94/6399a4d27711a5ad2c9bf5cd_ben-sweet-2LowviVHZ-E-unsplash-1.jpeg" />
+                <Card.Body>
+                    <Card.Title>Card Title</Card.Title>
+                    <Card.Text>
+                    Some quick example text to build on the card title and make up the
+                    bulk of the card's content.
+                    </Card.Text>
+                    <Button variant="success" className="w-100">Go somewhere</Button>
+                </Card.Body>
+            </Card>
+            <Card style={{ width: '18rem' }} className="flex-shrink-0">
+                <Card.Img variant="top" src="https://cdn.prod.website-files.com/62d84e447b4f9e7263d31e94/6399a4d27711a5ad2c9bf5cd_ben-sweet-2LowviVHZ-E-unsplash-1.jpeg" />
+                <Card.Body>
+                    <Card.Title>Card Title</Card.Title>
+                    <Card.Text>
+                    Some quick example text to build on the card title and make up the
+                    bulk of the card's content.
+                    </Card.Text>
+                    <Button variant="success" className="w-100">Go somewhere</Button>
+                </Card.Body>
+            </Card>
+            <Card style={{ width: '18rem' }} className="flex-shrink-0">
+                <Card.Img variant="top" src="https://cdn.prod.website-files.com/62d84e447b4f9e7263d31e94/6399a4d27711a5ad2c9bf5cd_ben-sweet-2LowviVHZ-E-unsplash-1.jpeg" />
+                <Card.Body>
+                    <Card.Title>Card Title</Card.Title>
+                    <Card.Text>
+                    Some quick example text to build on the card title and make up the
+                    bulk of the card's content.
+                    </Card.Text>
+                    <Button variant="success" className="w-100">Go somewhere</Button>
+                </Card.Body>
+            </Card>
+            <Card style={{ width: '18rem' }} className="flex-shrink-0">
+                <Card.Img variant="top" src="https://cdn.prod.website-files.com/62d84e447b4f9e7263d31e94/6399a4d27711a5ad2c9bf5cd_ben-sweet-2LowviVHZ-E-unsplash-1.jpeg" />
+                <Card.Body>
+                    <Card.Title>Card Title</Card.Title>
+                    <Card.Text>
+                    Some quick example text to build on the card title and make up the
+                    bulk of the card's content.
+                    </Card.Text>
+                    <Button variant="success" className="w-100">Go somewhere</Button>
+                </Card.Body>
+            </Card>
+            <Card style={{ width: '18rem' }} className="flex-shrink-0">
+                <Card.Img variant="top" src="https://cdn.prod.website-files.com/62d84e447b4f9e7263d31e94/6399a4d27711a5ad2c9bf5cd_ben-sweet-2LowviVHZ-E-unsplash-1.jpeg" />
+                <Card.Body>
+                    <Card.Title>Card Title</Card.Title>
+                    <Card.Text>
+                    Some quick example text to build on the card title and make up the
+                    bulk of the card's content.
+                    </Card.Text>
+                    <Button variant="success" className="w-100">Go somewhere</Button>
+                </Card.Body>
+            </Card>
+            <Card style={{ width: '18rem' }} className="flex-shrink-0">
+                <Card.Img variant="top" src="https://cdn.prod.website-files.com/62d84e447b4f9e7263d31e94/6399a4d27711a5ad2c9bf5cd_ben-sweet-2LowviVHZ-E-unsplash-1.jpeg" />
+                <Card.Body>
+                    <Card.Title>Card Title</Card.Title>
+                    <Card.Text>
+                    Some quick example text to build on the card title and make up the
+                    bulk of the card's content.
+                    </Card.Text>
+                    <Button variant="success" className="w-100">Go somewhere</Button>
+                </Card.Body>
+            </Card>
+            <Card style={{ width: '18rem' }} className="flex-shrink-0">
+                <Card.Img variant="top" src="https://cdn.prod.website-files.com/62d84e447b4f9e7263d31e94/6399a4d27711a5ad2c9bf5cd_ben-sweet-2LowviVHZ-E-unsplash-1.jpeg" />
+                <Card.Body>
+                    <Card.Title>Card Title</Card.Title>
+                    <Card.Text>
+                    Some quick example text to build on the card title and make up the
+                    bulk of the card's content.
+                    </Card.Text>
+                    <Button variant="success" className="w-100">Go somewhere</Button>
+                </Card.Body>
+            </Card>
+
+            <Card style={{ width: '18rem' }} className="flex-shrink-0">
+                <Card.Img variant="top" src="https://cdn.prod.website-files.com/62d84e447b4f9e7263d31e94/6399a4d27711a5ad2c9bf5cd_ben-sweet-2LowviVHZ-E-unsplash-1.jpeg" />
+                <Card.Body>
+                    <Card.Title>Card Title</Card.Title>
+                    <Card.Text>
+                    Some quick example text to build on the card title and make up the
+                    bulk of the card's content.
+                    </Card.Text>
+                    <Button variant="success" className="w-100">Go somewhere</Button>
+                </Card.Body>
+            </Card>
+            <Card style={{ width: '18rem' }} className="flex-shrink-0">
+                <Card.Img variant="top" src="https://cdn.prod.website-files.com/62d84e447b4f9e7263d31e94/6399a4d27711a5ad2c9bf5cd_ben-sweet-2LowviVHZ-E-unsplash-1.jpeg" />
+                <Card.Body>
+                    <Card.Title>Card Title</Card.Title>
+                    <Card.Text>
+                    Some quick example text to build on the card title and make up the
+                    bulk of the card's content.
+                    </Card.Text>
+                    <Button variant="success" className="w-100">Go somewhere</Button>
+                </Card.Body>
+            </Card>
+            <Card style={{ width: '18rem' }} className="flex-shrink-0">
+                <Card.Img variant="top" src="https://cdn.prod.website-files.com/62d84e447b4f9e7263d31e94/6399a4d27711a5ad2c9bf5cd_ben-sweet-2LowviVHZ-E-unsplash-1.jpeg" />
+                <Card.Body>
+                    <Card.Title>Card Title</Card.Title>
+                    <Card.Text>
+                    Some quick example text to build on the card title and make up the
+                    bulk of the card's content.
+                    </Card.Text>
+                    <Button variant="success" className="w-100">Go somewhere</Button>
+                </Card.Body>
+            </Card>
+        </div>
+    </Fragment>
 }
 
 export default App
--- React/src/pages/HomePage.jsx
+++ React/src/pages/HomePage.jsx
@@ -3,7 +3,7 @@ import Header from "../components/Header"
 
 const HomePage = () => {
     return <Fragment>
-        <Header />
+        <Header /> 
     </Fragment>
 }
 
