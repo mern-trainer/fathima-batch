7bb99811f1031aa1252a93f264f07d1cbacb4731
MERN-TRAINER <futuralabs.mern@gmail.com>
2025-05-03 10:29:59 +0530

    Nested Routes

--- React/src/App.jsx
+++ React/src/App.jsx
@@ -5,10 +5,28 @@ import Error404 from "./pages/Error404"
 
 const App = () => {
     return <BrowserRouter>
-        <Routes>
+        {/* <Routes>
             <Route path="/todo" Component={TodoList} />
             <Route path="/" Component={HomePage} />
+            <Route path="/user" element={<div>USER PAGE</div>} />
+            <Route path="/user/settings" element={<div>USER SETTINGS</div>} />
+            <Route path="/user/profile" element={<div>USER PROFILE</div>} />
             <Route path="*" Component={Error404} />
+        </Routes> */}
+
+        {/* Nested Routes */}
+
+        <Routes>
+            <Route path="/">
+                <Route path="" Component={HomePage} />
+                <Route path="todo" Component={TodoList} />
+                <Route path="user">
+                    <Route path="" element={<div>USER PAGE</div>} />
+                    <Route path="settings" element={<div>USER SETTINGS</div>} />
+                    <Route path="profile" element={<div>USER PROFILE</div>} />
+                </Route>
+                <Route path="*" Component={Error404} />
+            </Route>
         </Routes>
     </BrowserRouter>
 }
