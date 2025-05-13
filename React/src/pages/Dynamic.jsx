97916ef0b4626c1a742a58bb652fbd7de5d1374f
MERN-TRAINER <futuralabs.mern@gmail.com>
2025-05-13 10:15:03 +0530

    useNavigate

--- React/src/pages/Dynamic.jsx
+++ React/src/pages/Dynamic.jsx
@@ -1,11 +1,23 @@
-import { useParams } from "react-router"
+import { useNavigate, useParams } from "react-router"
 
 const Dynamic = () => {
 
     const { random_id } = useParams()
+    const navigate = useNavigate()
+    const is_authenticated = true;
+
+    const handleNavigate = () => {
+        if (!is_authenticated) {
+            return alert("Invalid User")
+        }
+        const randomNumber = Math.random()
+        navigate(`/dynamic/${randomNumber}`)
+    }
 
     return <div>
         Dynamic -- {random_id || "NO ID"}
+
+        <button onClick={handleNavigate}>Generate Random Number</button>
     </div>
 }
 
