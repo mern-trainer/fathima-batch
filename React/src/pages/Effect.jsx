ff767d12326b6bd014819cf1c95172e9a4439d57
MERN-TRAINER <futuralabs.mern@gmail.com>
2025-05-17 10:24:37 +0530

    api calling

--- React/src/pages/Effect.jsx
+++ React/src/pages/Effect.jsx
@@ -10,10 +10,13 @@ import { useEffect, useState } from "react"
 // Updating -> Called when the component is updated
 // Unmounting -> Called when the component is removed from the DOM
 
+// API Calling -> useEffect
+
 const EffectPage = () => {
 
     const [counter, setCounter] = useState(0)
     const [timer, setTimer] = useState(0)
+    const [users, setUsers] = useState([])
 
     // // Mounting
     // useEffect(() => {
@@ -44,11 +47,41 @@ const EffectPage = () => {
         }
     }, [counter])
 
+    const getUsers = async () => {
+        try{
+            const response = await fetch(`https://dummyjson.com/users`)
+            const res = await response.json()
+            setUsers(res.users);
+            console.log(res.users);
+        } catch (err) {
+            console.log(err);
+        }
+    }
+
+    useEffect(() => {
+        getUsers()
+    }, [])
+
     return <div>
         <h1>Effect Page</h1>
         <div>{counter} - {timer}</div>
         <button onClick={() => setCounter(counter + 1)}>Update</button>
         <button onClick={() => setTimer(timer + 1)}>Timer</button>
+
+        <div className="d-flex gap-2 flex-wrap">
+            {
+                users.map(user => {
+                    return <div key={user.id} className="d-flex gap-2 align-items-center p-1">
+                        <div>
+                            <img src={user.image} alt={user.firstName} width={40} height={40} />
+                        </div>
+                        <div>
+                            <h5>{user.firstName} {user.lastName}</h5>
+                        </div>
+                    </div>
+                })
+            }
+        </div>
     </div>
 }
 
