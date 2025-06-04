10b93b0c27b480fab82607f631edd982cf5f19b8
VISHNU M K <futuralabs.mern@gmail.com>
2025-06-04 16:47:05 +0530

    render file using nodejs

--- NodeJS/index.js
+++ NodeJS/index.js
@@ -1,14 +1,43 @@
 // http
 
 const http = require("http");
+const { readFileSync } = require("fs");
 
 const server = http.createServer()
+
+// 200 -> sucess
+// 201 -> created
+// 400 -> bad request
+// 404 -> not found
+// 401 -> unauthorized
+// 409 -> conflict
+// 500 -> internal server error
 
 server.on("request", (req, res) => {
     const path = req.url;
 
     if (path == "/") {
-        return res.end("Welcome to home page")
+        const indexFile = readFileSync("./index.html", { encoding: "utf-8" });
+        res.writeHead(200, {
+            "content-type": "text/html"
+        })
+        return res.end(indexFile)
+    }
+
+    if (path == "/about") {
+        const aboutFile = readFileSync("./about.html", { encoding: "utf-8" });
+        res.writeHead(200, {
+            "content-type": "text/html"
+        })
+        return res.end(aboutFile)
+    }
+
+    if(path == "/contact") {
+        const contactFile = readFileSync("./contact.html", { encoding: "utf-8" });
+        res.writeHead(200, {
+            "content-type": "text/html"
+        })
+        return res.end(contactFile)
     }
 
     return res.end("Page Not Found!")
